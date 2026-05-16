c48469508893cd53742de0d6467f0aa387152dd9
icecle <myersmjustin@yahoo.com>
2026-05-15 22:11:34 -0500

    completed gameboard class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,77 @@
+import Ship from "./ship.js";
+
 export default class Gameboard{
     constructor() {
-        this.board = 
+        this.board = [];
+    }
+
+    createBoard() {
+        this.board = Array.from({length: 10}, () => Array(10).fill(null));
+    }
+
+    isInBounds(posY, posX) {
+        return posY >= 0 && posY < 10 && posX >= 0 && posX < 10;
+    }
+
+    canPlaceShip(posY, posX, length, orientation) {
+        for (let i = 0; i < length; i++){
+            let y = posY;
+            let x = posX;
+
+            if (orientation === "horizontal") x = posX + i;
+            if (orientation === "vertical") y = posY + i;
+
+            if (!this.isInBounds(y, x)) return false;
+            if (this.board[y][x] !== null) return false;
+        }
+
+        return true;
+    }
+
+    placeShip(posY, posX, length, orientation) {
+        if (!this.canPlaceShip(posY, posX, length, orientation)) return false;
+
+        let ship = new Ship(length, orientation);
+
+        if (orientation === "horizontal") {
+            for (let i = 0; i < length; i++){
+                this.board[posY][posX + i] = ship; 
+            }
+        }
+
+        if (orientation === "vertical") {
+            for (let i = 0; i < length; i++){
+                this.board[posY + i][posX] = ship; 
+            }
+        }
+
+        return true;
+    }
+
+    receiveAttack(posY, posX) {
+        if (!this.isInBounds(posY, posX)) return false;
+
+        const target = this.board[posY][posX];
+
+        if (target instanceof Ship) {
+            target.hit();
+            return "hit";
+        }
+
+        this.board[posY][posX] = "miss";
+        return "miss";
+    }
+
+    checkShips() {
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[0].length; j++) {
+
+                let target = this.board[i][j];
+                if (target instanceof Ship) {
+                    if (!target.isSunk()) return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,3 +1,4 @@
+import Gameboard from "../gameboard.js";
 import Ship from "../ship.js";
 
 describe('check if ship has sunk', () => {
@@ -12,3 +13,71 @@ describe('check if ship has sunk', () => {
         expect(ship.isSunk()).toBe(true);
     });
 });
+
+describe('tests for the gameboard', () => {
+    let gameBoard;
+
+    beforeEach(() => {
+        gameBoard = new Gameboard();
+        gameBoard.createBoard();
+    });
+
+    test("check size of the outside array to be 10",() => {
+        expect(gameBoard.board.length).toBe(10);
+    });
+    test("check size of the inside array to be 10",() => {
+        expect(gameBoard.board[0].length).toBe(10);
+    });
+
+    test("placeShip fills all horizontal cells", () => {
+        const placed = gameBoard.placeShip(0, 2, 4, "horizontal");
+        const ship = gameBoard.board[0][2];
+
+        expect(placed).toBe(true);
+        expect(ship).not.toBeNull();
+        expect(gameBoard.board[0][3]).toBe(ship);
+        expect(gameBoard.board[0][4]).toBe(ship);
+        expect(gameBoard.board[0][5]).toBe(ship);
+    });
+
+    test("placeShip blocks overlap", () => {
+        gameBoard.placeShip(1, 1, 3, "vertical");
+        const placed = gameBoard.placeShip(2, 1, 2, "horizontal");
+
+        expect(placed).toBe(false);
+    });
+
+    test("placeShip blocks out-of-bounds", () => {
+        const placed = gameBoard.placeShip(9, 8, 3, "horizontal");
+
+        expect(placed).toBe(false);
+    });
+
+    test("receiveAttack registers a hit", () => {
+        gameBoard.placeShip(3, 3, 2, "horizontal");
+        const result = gameBoard.receiveAttack(3, 4);
+
+        expect(result).toBe("hit");
+    });
+
+    test("receiveAttack marks a miss", () => {
+        const result = gameBoard.receiveAttack(0, 0);
+
+        expect(result).toBe("miss");
+        expect(gameBoard.board[0][0]).toBe("miss");
+    });
+
+    test("checkShips returns false when a ship is still afloat", () => {
+        gameBoard.placeShip(5, 5, 2, "vertical");
+
+        expect(gameBoard.checkShips()).toBe(false);
+    });
+
+    test("checkShips returns true when all ships are sunk", () => {
+        gameBoard.placeShip(2, 2, 2, "horizontal");
+        gameBoard.receiveAttack(2, 2);
+        gameBoard.receiveAttack(2, 3);
+
+        expect(gameBoard.checkShips()).toBe(true);
+    });
+})
