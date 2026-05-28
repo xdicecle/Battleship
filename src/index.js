48f8eca1365e792a7e4cef1f197dbff90f392840
icecle <myersmjustin@yahoo.com>
2026-05-27 19:12:46 -0500

    feat: added the ability to view ships on the player's gameboard and the computer's gameboard, as well as added hover effects to the squares on both gameboards. Also added a gap between the two gameboards on larger screens.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+//Imports
 import Ship from "./ship";
+import Gameboard from "./gameboard";
 import "./style.css";
-import { createGameBoard } from "../ui";
+import { createGameBoard, placeShip } from "../ui";
 
+//Initialize variables
 const playerBoard = document.getElementById("gameboardPlayer");
 const computerBoard = document.getElementById("gameboardComputer");
+const playerGameBoard = new Gameboard();
+const computerGameBoard = new Gameboard();
 
+// Create and display game boards
 createGameBoard(playerBoard);
 createGameBoard(computerBoard);
+
+// Initialize boards
+playerGameBoard.createBoard();
+computerGameBoard.createBoard();
+
+// Place ships on player's board
+placeShip(0, 0, 4, "horizontal", playerBoard, playerGameBoard); // Battleship
+placeShip(2, 0, 3, "horizontal", playerBoard, playerGameBoard); // Cruiser
+placeShip(4, 0, 2, "vertical", playerBoard, playerGameBoard); // Destroyer
+placeShip(6, 0, 2, "horizontal", playerBoard, playerGameBoard); // Submarine
+
+// Place ships on computer's board
+placeShip(1, 1, 4, "vertical", computerBoard, computerGameBoard); // Battleship
+placeShip(1, 3, 3, "horizontal", computerBoard, computerGameBoard); // Cruiser
+placeShip(4, 2, 2, "vertical", computerBoard, computerGameBoard); // Destroyer
+placeShip(7, 5, 2, "horizontal", computerBoard, computerGameBoard); // Submarine
--- ui.js
+++ ui.js
@@ -1,18 +1,54 @@
+//imports
+import Gameboard from "./src/gameboard";
+import Ship from "./src/ship";
 
 // Adds 100 boxes to the gameboard containers
 export function createGameBoard(board) {
-    for (let y = 0; y < 10; y++){
-        for (let x = 0; x < 10; x++){
-            let inputBox = document.createElement("button");
-            inputBox.className = "square";
-            inputBox.addEventListener("mouseover", (e) => {
-                e.target.style.backgroundColor = "grey"
-            })
-            inputBox.addEventListener("mouseout", (e) => {
-                e.target.style.backgroundColor = "";
-            });
-            board.appendChild(inputBox);
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      const inputBox = document.createElement("button");
+      inputBox.className = "square";
+
+      // change button colors on hover
+      inputBox.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "grey";
+      });
+      inputBox.addEventListener("mouseout", (e) => {
+        e.target.style.backgroundColor = "";
+      });
+
+      // assign data attributes to identify boxes
+      inputBox.dataset.x = x;
+      inputBox.dataset.y = y;
+
+      inputBox.addEventListener("click", (e) => {
+        const x = Number(e.currentTarget.dataset.x);
+        const y = Number(e.currentTarget.dataset.y);
+
+        console.log("board position:", y, x);
+      });
+
+      board.appendChild(inputBox);
     }
-    }
+  }
 }
 
+function displayShip(posY, posX, length, orientation, board) {
+  if (orientation === "vertical") {
+    for (let y = posY; y < posY + length; y++) {
+      let box = board.querySelector(`[data-x="${posX}"][data-y="${y}"]`);
+      box.style.backgroundColor = "purple";
+    }
+  } else if (orientation === "horizontal") {
+    for (let x = posX; x < posX + length; x++) {
+      let box = board.querySelector(`[data-x="${x}"][data-y="${posY}"]`);
+      box.style.backgroundColor = "purple";
+    }
+  }
+}
+
+export function placeShip(posY, posX, length, orientation, board, gameBoard) {
+  if (!gameBoard.placeShip(posY, posX, length, orientation)) return false;
+
+  displayShip(posY, posX, length, orientation, board);
+}
